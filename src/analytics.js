47374cd119078eb0cc96a180c646df7fa7a2b206
Luc Yuki Marrie <lucmarrie1@gmail.com>
2022-03-06 15:07:38 -0500

    analytics partial updates for entityIds

--- src/analytics.js
+++ src/analytics.js
@@ -14,7 +14,7 @@ function pixelURL(data) {
   return p;
 }
 
-window.trackAnalytics = (businessId, siteId) => {
+window.trackAnalytics = (businessId, siteId, entityId) => {
   if (isStaging()) {
     return;
   }
@@ -25,6 +25,7 @@ window.trackAnalytics = (businessId, siteId) => {
     'eventType': "pageview",
     'businessids': businessId,
     'siteId': siteId,
+    'ids': entityId,
   };
     
   var pixel = pixelURL(jsonData);
